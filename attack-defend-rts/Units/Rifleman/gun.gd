d1a03daa99e7079e053b11b029febf6739c6d32e
MrForkle <Aaron_Makes_Games@proton.me>
2024-11-28 08:45:21 -0500

    updated aiming

--- attack-defend-rts/Units/Rifleman/gun.gd
+++ attack-defend-rts/Units/Rifleman/gun.gd
@@ -7,10 +7,12 @@ var looks_at = null
 var can_shoot_at = []
 var cycle :int = 0
 
-func _process(_delta):
+func update_aiming():
 	
-	if get_parent().is_in_group("Placing"): return
+	can_shoot_at = []
+	
 	var in_range = get_parent().get_node("Detection Range").get_overlapping_bodies()
+	
 	for i in in_range:
 		var space = get_world_3d().direct_space_state
 		var ray_query = PhysicsRayQueryParameters3D.new()
@@ -22,17 +24,24 @@ func _process(_delta):
 			pass
 		elif instance_from_id(raycast_result['collider_id']) == i:
 			can_shoot_at.append(i)
-	if !can_shoot_at.is_empty() and looks_at != null:
+	if !can_shoot_at.is_empty():
+		print("aiming")
+		looks_at = randi_range(0,len(can_shoot_at)-1)
 		shooting = true
 		var look_at_position = can_shoot_at[looks_at].global_position
 		get_parent().look_at(look_at_position)
 		look_at(look_at_position)
 	else:
 		shooting = false
-	can_shoot_at = []
+	
+
+func _process(_delta):
+	
+	if get_parent().is_in_group("Placing"): return
+	
+	update_aiming()
 	
 	if cycle >= 30:
-		looks_at = randi_range(0,len(can_shoot_at)-1)
 		cycle = 0
 		if shooting == true:
 			var bullet_instantiated := bullet.instantiate()
