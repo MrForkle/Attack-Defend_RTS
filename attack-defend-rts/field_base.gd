4b053c7bcdf9824a5cabf8683fa3d3f42bf64a82
MrForkle <Aaron_Makes_Games@proton.me>
2025-01-01 19:02:46 -0500

    fixed unit spawning issues

--- attack-defend-rts/field_base.gd
+++ attack-defend-rts/field_base.gd
@@ -17,7 +17,7 @@ func deselect():
 func spawn_unit(unit,team):
 	unit = units[unit]
 	var unit_instantiated = unit.instantiate()
-	unit_instantiated.position = $"Spawn Point".position
+	unit_instantiated.position = $"Spawn Point".global_position
 	var color = null
 	if team == "red":
 		color = get_tree().get_root().get_node("Map").blue_material
